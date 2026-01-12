f8b2c8bfe01a65858b3648a259230560a663f591
Vaishnavik-99 <vaishnavi.kunnampilly@gmail.com>
2026-01-12 19:50:03 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -7,4 +7,8 @@ scaler = joblib.load("../models/scaler.pkl")
 def predict_cluster(age, income, score):
     data = np.array([[age, income, score]])
     scaled = scaler.transform(data)
-    return model.predict(scaled)[0]
+    cluster = model.predict(scaled)
+    return cluster[0]
+
+if __name__ == "__main__":
+    print(predict_cluster(25, 40, 60))
